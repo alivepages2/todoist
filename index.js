fdb6ba85534f799bbb673c640d86e39312a04e2f
“alivepages2” <ventas@livepages.com>
2021-05-28 11:07:22 -0500

    login in todoist

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const puppeteer = require("puppeteer");
-function run() {
+
+// Pull a list of tasks from https://randomtodolistgenerator.herokuapp.com/library
+function getTasks() {
   return new Promise(async (resolve, reject) => {
     try {
-      const browser = await puppeteer.launch();
+      //const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 1,
+        defaultViewport: null,
+      });
       const page = await browser.newPage();
-      const url = "";
       await page.goto("https://randomtodolistgenerator.herokuapp.com/library");
       let urls = await page.evaluate(() => {
         let results = [];
         let items = document.querySelectorAll(".taskCard");
         items.forEach((item, index) => {
-          if (index < 4)
-            results.push({
-              text: item.innerText.split("\n"),
-            });
+          results.push({
+            text: item.innerText.split("\n"),
+          });
         });
         return results;
       });
@@ -24,4 +29,30 @@ function run() {
     }
   });
 }
-run().then(console.log).catch(console.error);
+
+function login(value) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser1 = await puppeteer.launch({
+        headless: false,
+        slowMo: 1,
+        defaultViewport: null,
+      });
+
+      const page1 = await browser1.newPage();
+      await page1.goto("https://todoist.com/users/showlogin");
+
+      await page1.type("#email", "soporte@alivepages.com");
+      await page1.type("#password", "alive+123");
+      await page1.click("button");
+      await page1.waitForSelector(".icon_add");
+
+      //browser.close();
+      //return resolve(urls);
+    } catch (e) {
+      return reject(e);
+    }
+  });
+}
+getTasks().then(login()).catch(console.error);
+//login().catch(console.error);
